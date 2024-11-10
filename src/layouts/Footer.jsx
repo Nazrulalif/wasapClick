54161371d60abf0baf3bd44fd027bb2b140ac933
Nazrulalif <pumaddidaszero@gmail.com>
2024-11-10 14:11:11 +0800

    footer added

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="pl-3 lg:p-5 py-2 bottom-0 z-1 bg-white opacity-60">
+        <div className="flex flex-col items-center">
+          <div className="font-mono text-xs md:text-sm lg:text-sm  text-center">
+            © 2024 wasapclick.vercel.app, All rights reserved.
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-
+import Footer from "./Footer";
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
