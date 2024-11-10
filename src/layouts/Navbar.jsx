ed1f7d088e3f4691a79091efab56b39666a94c2f
Nazrulalif <pumaddidaszero@gmail.com>
2024-11-10 20:24:25 +0800

    make navbar center sm

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <>
       <nav className="p-1 lg:p-3 py-5 lg:py-7 bg-white sticky top-0 z-50 shadow-sm">
         <a href="/">
-          <div className="ml-3 lg:ml-36 flex items-center opacity-70">
-            <i className="fab fa-whatsapp mr-2 lg:mr-4 text-3xl lg:text-3xl"></i>
+          <div className="ml-3 lg:ml-36 flex items-center opacity-70 justify-center lg:justify-start md:justify-start">
+            <i className="fab fa-whatsapp mr-2 lg:mr-4 text-3xl lg:text-3xl "></i>
             <span className="font-mono font-bold text-sm lg:text-xl tracking-wider">
               WasapClick!
             </span>
